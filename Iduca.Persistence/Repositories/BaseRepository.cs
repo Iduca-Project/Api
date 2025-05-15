8822d55b9518e7a4dfcb034a082932a441257ef1
AmiltonMn <amilton.moreiran@hotmail.com>
2025-05-15 17:16:04 -0300

    feat: Clean architecture #8 BaseRepository with another actions

--- Iduca.Persistence/Repositories/BaseRepository.cs
+++ Iduca.Persistence/Repositories/BaseRepository.cs
@@ -14,6 +14,29 @@ public class BaseRepository<TModel>(IducaContext Context) : IBaseRepository<TMod
 
     public void Update(TModel entity)
         => context.Update(entity);
+
+    public Task<TModel?> Get(Guid id, CancellationToken cancellationToken)
+        => context
+            .Set<TModel>()
+            .Where(entity => entity.DisabledAt == null)
+            .FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken);
+
+    public Task<List<TModel>> GetAll(CancellationToken cancellationToken)
+        => context
+            .Set<TModel>()
+            .Where(entity => entity.DisabledAt == null)
+            .ToListAsync(cancellationToken);
+
+    public void Delete(TModel entity)
+    {
+        entity.DisabledAt = DateTime.UtcNow;
+        context.Update(entity);
+    }
     
-    
+    public Task<bool> Exists(Guid id, CancellationToken cancellationToken)
+        => dbSet.AnyAsync(e => 
+            EF.Property<Guid>(e, "Id") == id && 
+            EF.Property<Guid?>(e, "DeletedAt") == null, 
+            cancellationToken
+        );
 }
