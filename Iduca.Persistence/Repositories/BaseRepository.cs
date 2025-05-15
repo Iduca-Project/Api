02f439d34bb7c8dc96a1afb7596151392fbf7163
CrCardd <cris.rc377@gmail.com>
2025-05-15 16:48:31 -0300

    feat #8 fix syntax errors

--- Iduca.Persistence/Repositories/BaseRepository.cs
+++ Iduca.Persistence/Repositories/BaseRepository.cs
@@ -6,8 +6,8 @@ using Iduca.Domain.Models;
 public class BaseRepository<TModel>(IducaContext Context) : IBaseRepository<TModel>
     where TModel : BaseModel
 {
-    protected readonly IDucaContext context = Context;
-    protected readonly DbSet<TModel> dbSet = IDucaContext.Set<TModel>();
+    protected readonly IducaContext context = Context;
+    protected readonly DbSet<TModel> dbSet = context.Set<TModel>();
 
     public void Create(TModel entity)
         => context.Add(entity);
