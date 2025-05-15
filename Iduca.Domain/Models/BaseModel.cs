2e2207cd886e266946814e1db8c5cd61b7002920
AmiltonMn <amilton.moreiran@hotmail.com>
2025-05-15 16:59:28 -0300

    feat: Clean architecture #8 All structure changing errors were fixed

--- Iduca.Domain/Models/BaseModel.cs
+++ Iduca.Domain/Models/BaseModel.cs
@@ -2,7 +2,7 @@ namespace Iduca.Domain.Models;
 
 public class BaseModel
 {
-    public Guid id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; set; } = Guid.NewGuid();
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? DisabledAt { get; set; } = null;
