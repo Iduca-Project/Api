a16db3e6986b9d9ac28130a121b505d3481f1477
AmiltonMn <amilton.moreiran@hotmail.com>
2025-06-09 16:09:44 -0300

    feat: #27

--- Iduca.Api/Enums/APIRoutes.cs
+++ Iduca.Api/Enums/APIRoutes.cs
@@ -11,4 +11,5 @@ public static class APIRoutes
     public const string Companies = "api/company";
     public const string Categories = "api/categories";
     public const string Modules = "api/modules";
+    public const string Lessons = "api/lessons";
 }
